205b3678fc73df7d0e1d3ef3e0c13f9fc56036d7
Disha-56843 <dishajasani56843@gmail.com>
2024-05-22 17:42:59 +0530

    Upload

---
+++ Practice/Day1/src/practice.java
@@ -0,0 +1,7 @@
+
+public class practice {
+	public static void main(String[] argu) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ Practice/Practice2/src/Practice2.java
@@ -0,0 +1,8 @@
+
+public class Practice2 {
+	public static void main(String[] argu) {
+		int x = 5;
+		int y = 10; 
+		System.out.println(x * y);
+	}
+}
